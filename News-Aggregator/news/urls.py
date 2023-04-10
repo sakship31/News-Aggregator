016452529633049e315a256e9aed98f696c2eaf7
Merko <Merko@LAPTOP-3P627PH6>
2023-04-10 15:05:13 +0430

    Beatify Home Page & add Rss & refactor scrapper

--- News-Aggregator/news/urls.py
+++ News-Aggregator/news/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from news.views import scrape, news_list
+from news.views import scrape, news_list,rss_scrape
 urlpatterns = [
   path('scrape/', scrape, name="scrape"),
+  path('rss_scrape/', rss_scrape, name="rss_scrape"),
   path('', news_list, name="home"),
 ]
