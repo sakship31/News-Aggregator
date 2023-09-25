6c40ca70e05947d6e80679333e2919744955fa39
Shubham Boghara <shubhamboghara444@gmail.com>
2023-09-25 19:43:38 +0530

    Added: Multiple section news web scraper

--- News-Aggregator/news/views.py
+++ News-Aggregator/news/views.py
@@ -6,38 +6,39 @@ from news.models import Headline
 
 # Create your views here.
 
-def scrape(request):
-  Headline.objects.all().delete()
-  session = requests.Session()
-  session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
-  url = "https://www.theonion.com/latest"
-  content = session.get(url).content
-  soup = BSoup(content, "html.parser")
 
-  News = soup.find_all('div', {"class":"sc-cw4lnv-13 hHSpAQ"})
+def scrape(request, name):
+    Headline.objects.all().delete()
+    session = requests.Session()
+    session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
+    url = f"https://www.theonion.com/{name}"
+    content = session.get(url).content
+    soup = BSoup(content, "html.parser")
 
-  for article in News:
-    main = article.find_all('a',href=True)
- 
-    linkx = article.find('a', {"class":"sc-1out364-0 dPMosf js_link"})
-    link=linkx['href']    
-    
-    titlex = article.find('h2', {"class":"sc-759qgu-0 cvZkKd sc-cw4lnv-6 TLSoz"})
-    title = titlex.text
-    
-    imgx = article.find('img')["data-src"]
+    News = soup.find_all("div", {"class": "sc-cw4lnv-13 hHSpAQ"})
 
-    new_headline = Headline()
-    new_headline.title = title
-    new_headline.url = link
-    new_headline.image = imgx
-    new_headline.save()
-  return redirect("../")
+    for article in News:
+        main = article.find_all("a", href=True)
+
+        linkx = article.find("a", {"class": "sc-1out364-0 dPMosf js_link"})
+        link = linkx["href"]
+
+        titlex = article.find("h2", {"class": "sc-759qgu-0 cvZkKd sc-cw4lnv-6 TLSoz"})
+        title = titlex.text
+
+        imgx = article.find("img")["data-src"]
+
+        new_headline = Headline()
+        new_headline.title = title
+        new_headline.url = link
+        new_headline.image = imgx
+        new_headline.save()
+    return redirect("../")
 
 
 def news_list(request):
     headlines = Headline.objects.all()[::-1]
     context = {
-        'object_list': headlines,
+        "object_list": headlines,
     }
     return render(request, "news/home.html", context)
